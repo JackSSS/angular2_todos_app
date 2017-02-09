d747d4e5ef3bf39d36b9e6ef4e03446108a7f3ec
Jack Sneed <JackSSS@users.noreply.github.com>
2017-02-08 22:59:44 -0800

    set up api

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var todos = require('./routes/todos');
+
+var app = express();
+
+//VIEW ENGINE
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/', index);
+app.use('/api/v1', todos);
+
+app.listen(3000, function(){
+    console.log('Server listening on port 3000...');
+})
