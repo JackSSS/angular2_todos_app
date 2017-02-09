8470dcb603d35e43e6b2dc4ad888c88e5a369b12
Jack Sneed <JackSSS@users.noreply.github.com>
2017-02-09 15:08:06 -0800

    add angular 2

---
+++ client/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>Your first Angular 2 app</h1>'
+})
+
+export class AppComponent { }
--- server.js
+++ server.js
@@ -11,12 +11,15 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/', index);
-app.use('/api/v1', todos);
+app.use('/api/v1/', todos);
 
 app.listen(3000, function(){
     console.log('Server listening on port 3000...');
